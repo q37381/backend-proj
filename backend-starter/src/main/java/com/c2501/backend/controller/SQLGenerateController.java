74eaf28ab8751c37f0c5b8dcf14c2df9a1e026d0
W <373814603@qq.com>
2021-04-26 23:58:10 +0800

    加sql

--- backend-starter/src/main/java/com/c2501/backend/controller/SQLGenerateController.java
+++ backend-starter/src/main/java/com/c2501/backend/controller/SQLGenerateController.java
@@ -5,18 +5,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.druid.util.StringUtils;
+import com.common.service.SqlGenerateService;
 import com.common.util.SQLToJavaFieldUtil;
 
 @RestController
-@RequestMapping("sqlToJava")
-public class SQLToJavaController {
+@RequestMapping("sqlGenerate")
+public class SQLGenerateController {
 
-    @PostMapping
+    // TODOM 后期改成接口调用
+    private SqlGenerateService sqlGenerateService = new SqlGenerateService();
+
+    @PostMapping("/toJava")
     public String sqlToJava(String sql) {
         if (StringUtils.isEmpty(sql)) {
             return null;
         }
+
         return SQLToJavaFieldUtil.getJavaFieldStr(sql);
 
+    }
+
+    @PostMapping("/wordToSql")
+    public String wordToSql(String word) {
+        if (StringUtils.isEmpty(word)) {
+            return null;
+        }
+
+        return sqlGenerateService.generateSqlByWord(word);
     }
 }
